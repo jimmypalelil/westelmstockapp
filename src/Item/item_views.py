5b55b883f964e032d19ea4c0fe1ad880adc04cab
jimmypalelil <30486617+jimmypalelil@users.noreply.github.com>
2018-08-23 19:54:28 -0700

    Add files via upload

--- src/Item/item_views.py
+++ src/Item/item_views.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template, request, redirect, url_for
+from flask import Blueprint, render_template, request, redirect, url_for, make_response
 
 from src.Item.item import Item
 
@@ -7,3 +7,9 @@ item_bp = Blueprint('item_blueprint', __name__)
 @item_bp.route('/new_item/<sku>/<desc>/<claimed>/<stock>', methods=['POST','GET'])
 def new_item(sku, desc, claimed, stock):
     Item(sku, desc, claimed, stock).save_to_mongo()
+
+@item_bp.route('/search_item/<id>', methods=['GET'])
+def search_item(id):
+    id = id[-4:]
+    item = Item.get_by_sku(id)
+    make_response(item.json())
