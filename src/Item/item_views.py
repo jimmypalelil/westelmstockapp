ff326a8f62a186395ddb6046a27cadd10b5a443a
jimmypalelil <30486617+jimmypalelil@users.noreply.github.com>
2018-08-25 13:36:57 -0700

    Add files via upload

--- src/Item/item_views.py
+++ src/Item/item_views.py
@@ -1,9 +1,19 @@
-from flask import Blueprint, render_template, request, redirect, url_for
+from flask import Blueprint, render_template, request, redirect, url_for, make_response
+from bson.json_util import dumps
 
 from src.Item.item import Item
 
 item_bp = Blueprint('item_blueprint', __name__)
 
-@item_bp.route('/new_item/<sku>/<desc>', methods=['POST','GET'])
-def new_item(sku, desc):
-    Item(sku, desc).save_to_mongo()
+@item_bp.route('/new_item/<sku>/<desc>/<claimed>/<stock>/<price>', methods=['POST','GET'])
+def new_item(sku, desc, claimed, stock, price):
+    Item(sku, desc, claimed, stock, price).save_to_mongo()
+
+@item_bp.route('/search_item/<id>', methods=['GET','POST'])
+def search_item(id):
+    id = id[-4:]
+    item = Item.get_by_id(id)
+    if (item is None):
+        return "Null"
+    else:
+        return dumps(item.json())
