eb29bfd7e380eeaf88e63be426a2a9a7e8d91461
jimmypalelil <30486617+jimmypalelil@users.noreply.github.com>
2018-08-25 13:26:16 -0700

    Add files via upload

--- src/Item/item.py
+++ src/Item/item.py
@@ -3,29 +3,21 @@ import uuid
 from src.common.database import Database
 
 class Item():
-    def __init__(self, sku, desc, claimed, stock, _id=None):
+    def __init__(self, sku, desc, _id=None):
         self.desc = desc
         self.sku = sku
-        self.claimed = claimed
-        self.stock = stock
         self._id = sku[-4:]
 
     @classmethod
-    def get_by_id(cls, id):
-        item = Database.find_one('items', {'_id': id})
-        if(item is None):
-            return None
-        else:
-            return cls(**item)
+    def get_by_sku(cls, sku):
+        return cls(**Database.find_one('items', {'sku': sku}))
 
     def json(self):
         return {
             "_id": self._id,
             "sku": self.sku,
-            "desc": self.desc,
-            "claimed": self.claimed,
-            "stock": self.stock
+            "desc": self.desc
         }
 
     def save_to_mongo(self):
-        Database.update('items', {"_id": self._id}, self.json())
+        Database.insert('items', self.json())
--- src/Item/item_views.py
+++ src/Item/item_views.py
@@ -1,19 +1,9 @@
-from flask import Blueprint, render_template, request, redirect, url_for, make_response
-from bson.json_util import dumps
+from flask import Blueprint, render_template, request, redirect, url_for
 
 from src.Item.item import Item
 
 item_bp = Blueprint('item_blueprint', __name__)
 
-@item_bp.route('/new_item/<sku>/<desc>/<claimed>/<stock>', methods=['POST','GET'])
-def new_item(sku, desc, claimed, stock):
-    Item(sku, desc, claimed, stock).save_to_mongo()
-
-@item_bp.route('/search_item/<id>', methods=['GET','POST'])
-def search_item(id):
-    id = id[-4:]
-    item = Item.get_by_id(id)
-    if (item is None):
-        return "Null"
-    else:
-        return dumps(item.json())
+@item_bp.route('/new_item/<sku>/<desc>', methods=['POST','GET'])
+def new_item(sku, desc):
+    Item(sku, desc).save_to_mongo()
