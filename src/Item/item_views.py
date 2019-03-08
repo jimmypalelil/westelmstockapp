1e6301703c23eece48613d973d8cf043c7896d9b
Jimmy Palelil <jimmypalelil@gmail.com>
2019-03-08 11:37:58 -0800

    getting item in string format for json

--- src/Item/item_views.py
+++ src/Item/item_views.py
@@ -5,16 +5,18 @@ from src.Item.item import Item
 
 item_bp = Blueprint('item_blueprint', __name__)
 
+
 @item_bp.route('/new_item/<sku>/<desc>/<claimed>/<stock>/<price>', methods=['POST','GET'])
 def new_item(sku, desc, claimed, stock, price):
     if request.method == 'POST':
         Item(sku, desc, claimed, stock, price).save_to_mongo()
 
-@item_bp.route('/search_item/<id>', methods=['GET','POST'])
-def search_item(id):
-    id = id[-4:]
-    item = Item.get_by_id(id)
-    if (item is None):
+
+@item_bp.route('/search_item/<item_id>', methods=['GET','POST'])
+def search_item(item_id):
+    item_id = item_id[-4:]
+    item = Item.get_by_id(item_id)
+    if item is None:
         return "Null"
     else:
         return dumps(item.json())
