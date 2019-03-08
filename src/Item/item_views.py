84148052619a24e2e8b6cd15d93bb0d6bf412ac1
Jimmy Palelil <jimmypalelil@gmail.com>
2019-03-08 10:04:34 -0800

    added search item function

--- src/Item/item_views.py
+++ src/Item/item_views.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template, request, redirect, url_for
+from flask import Blueprint, render_template, request, redirect, url_for, jsonify
 
 from src.Item.item import Item
 
@@ -7,3 +7,7 @@ item_bp = Blueprint('item_blueprint', __name__)
 @item_bp.route('/new_item/<sku>/<desc>', methods=['POST','GET'])
 def new_item(sku, desc):
     Item(sku, desc).save_to_mongo()
+
+@item_bp.route('/search_item/<sku>')
+def search_item(sku):
+    return jsonify(Item.get_by_sku(sku))
